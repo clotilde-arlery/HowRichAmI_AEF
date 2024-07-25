dd9ccdb6fa2756d328dc80126c9776ec5f84c6cc
clotilde-arlery <68501578+clotilde-arlery@users.noreply.github.com>
2024-07-25 16:21:24 +0200

    Update main.js
    debug income position bar size

--- main.js
+++ main.js
@@ -350,7 +350,9 @@ function App(triggeringEvent){
     let graph_container = document.getElementById('graph_container');
     let income_position_bar = document.getElementById('income_position_bar');
     let graph_width = graph_container.clientWidth;
-    income_position_bar.style.width = ((1 - percentile/100)*graph_width).toString()+"px";
+    let poorer_graph_width = document.getElementById('poorer_position');
+    income_position_bar.style.width = ((1 - percentile/100)*graph_width).toString()+"%";
+    //poorer_graph_width.style.width = 
     let revenuMedian = 6499.6;
     
     let timesRevenuMedian = ajusted_income/revenuMedian;
